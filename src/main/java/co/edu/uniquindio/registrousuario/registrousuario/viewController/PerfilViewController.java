8f281f062237d474d12b62e789719bc56b3b6a4c
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-23 21:05:21 -0500

    comits crud

--- src/main/java/co/edu/uniquindio/registrousuario/registrousuario/viewController/PerfilViewController.java
+++ src/main/java/co/edu/uniquindio/registrousuario/registrousuario/viewController/PerfilViewController.java
@@ -1,9 +1,12 @@
 package co.edu.uniquindio.registrousuario.registrousuario.viewController;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -28,7 +31,13 @@ public class PerfilViewController {
 
     @FXML
     void onCerrarSesion(ActionEvent event) {
-
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/co/edu/uniquindio/registrousuario/registrousuario/Iniciosesion.fxml"));
+            Parent root = loader.load();
+            lblNombreUsuario.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
